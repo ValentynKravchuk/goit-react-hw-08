84ee2e90df9309224e1265bd06fd22fe2463b476
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2024-12-16 20:04:04 +0100

    updata json

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,10 @@
+import { LuBookHeadphones } from "react-icons/lu";
+
 export const HomePage = () => {
   return (
     <div>
       <h1>Welcome to the Contacts App!</h1>
+      <LuBookHeadphones />
     </div>
   );
 };
